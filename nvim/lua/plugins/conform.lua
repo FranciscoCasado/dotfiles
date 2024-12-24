c93bdd3abe8a8a9a39237a91bf4d0d56e0b906d2
Francisco Casado <hola@franciscocasado.cl>
2024-12-24 18:25:08 -0300

    chore(nvim): add python formatter

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -7,7 +7,7 @@ return {
     conform.setup({
       formatters_by_ft = {
         lua = { 'stylua' },
-        python = { 'ruff' },
+        python = { 'ruff_fix', 'ruff_format' },
       },
       format_on_save = { lsp_format = 'fallback' },
     })
