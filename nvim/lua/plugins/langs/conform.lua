3da6bdc2022c171d60b403d4bcd9ac7f3792ee10
Francisco Casado <hola@franciscocasado.cl>
2025-05-05 01:47:33 -0400

    feat: add fzf and live_grep_args teslecope extensions

--- nvim/lua/plugins/langs/conform.lua
+++ nvim/lua/plugins/langs/conform.lua
@@ -8,6 +8,7 @@ return {
       formatters_by_ft = {
         lua = { 'stylua' },
         python = { 'ruff_fix', 'ruff_format', 'ruff_organize_imports' },
+        json = { 'prettierd' },
       },
       format_on_save = { lsp_format = 'fallback' },
     })
--- nvim/lua/plugins/nav/telescope.lua
+++ nvim/lua/plugins/nav/telescope.lua
@@ -1,11 +1,32 @@
 return {
   'nvim-telescope/telescope.nvim',
   tag = '0.1.8',
-  dependencies = { 'nvim-lua/plenary.nvim' },
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    'nvim-telescope/telescope-live-grep-args.nvim',
+  },
+  config = function()
+    local telescope = require('telescope')
+
+    telescope.setup({
+      extensions = {
+        live_grep_args = {
+          file_ignore_patterns = {
+            '^.git/',
+            '%.lock$',
+            '*/__pycache__/',
+          },
+        },
+      },
+    })
+    telescope.load_extension('fzf')
+    telescope.load_extension('live_grep_args')
+  end,
   keys = function()
     return {
       { '<leader>ff', '<cmd>Telescope find_files<cr>', desc = 'Find Files' },
-      { '<leader>fg', '<cmd>Telescope live_grep<cr>', desc = 'Find Files' },
+      { '<leader>fg', '<cmd>Telescope live_grep<cr>', desc = 'Grep Files' },
     }
   end,
 }
