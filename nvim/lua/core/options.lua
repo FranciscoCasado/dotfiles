a39154928aee7e8cf875b1a58672a196105a0d5e
Francisco Casado <hola@franciscocasado.cl>
2025-06-17 00:21:21 -0400

    feat(nvim): add autopairs and bqf, also tweaked other plugins

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -7,6 +7,6 @@ vim.opt.expandtab = true
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
 
-vim.opt.wrap = true
+vim.opt.wrap = false
 
 vim.opt.clipboard = 'unnamed'
--- nvim/lua/plugins/edit/nvim-cmp.lua
+++ nvim/lua/plugins/edit/nvim-cmp.lua
@@ -21,5 +21,8 @@ return {
       }),
       completion = { completeopt = 'menu,menuone,noinsert' },
     })
+
+    local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+    cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
   end,
 }
