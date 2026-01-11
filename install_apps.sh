24b9491d08e67d347c195adb7183d7da49254040
Francisco Casado <hola@franciscocasado.cl>
2026-01-11 01:35:06 -0300

    add gpg

--- install_apps.sh
+++ install_apps.sh
@@ -2,6 +2,7 @@ brew install 1password-cli
 brew install colima
 brew install docker docker-compose && mkdir -p ~/.docker/cli-plugins/ && ln $(which docker-compose) ~/.docker/cli-plugins/
 brew install fzf
+brew install gnupg
 brew install --cask kitty
 brew install mise
 brew install neovim
